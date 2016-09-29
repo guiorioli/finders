68639e859e1b8a919519bc053f90ccf9e4a124c7
Guilherme <gui.orioli@gmail.com>
2016-09-29 18:01:58 -0300

    Added uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+uninstallscript(){
+	rm /usr/bin/$1
+}
+
+uninstallscript xfind
+uninstallscript ffind
+uninstallscript effind
+uninstallscript offind
+uninstallscript closeall
+uninstallscript e
+uninstallscript o
+uninstallscript killer
